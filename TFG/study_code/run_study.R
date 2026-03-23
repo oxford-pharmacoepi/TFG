a68397c6746d097d3cad50177ea28eab5fac2d4d
catalamarti <marticatalasabate@gmail.com>
2026-03-23 16:42:42 +0000

    add comments

--- TFG/study_code/run_study.R
+++ TFG/study_code/run_study.R
@@ -13,19 +13,21 @@ results <- list()
 # CDM modifications -----
 
 # CDM summary -----
+logMessage("Extract CDM snapshot") # MC
 results[["snapshot"]] <- summariseOmopSnapshot(cdm)
+
+logMessage("Extract observation period summary") # MC
 results[["obs_period"]] <- summariseObservationPeriod(cdm$observation_period)
 
 # Instantiate study cohorts ----
 logMessage("Instantiating study cohorts")
-source(here("codelist", "codelist_creation.R"))
+source(here("codelist", "codelist_creation.R")) # MC the code seems more to read the codelists than to create them no?
 source(here("cohorts", "functions.R"))
 source(here("cohorts", "instantiate_cohorts.R"))
-source(here("cohorts", "all.R"))
+source(here("cohorts", "all.R")) # MC does not exist
 logMessage("Study cohorts instantiated")
 
 # Cohort counts and attrition ----
-# results[["counts"]] <- summariseCohortCount("...")
 results[["attrition"]] <- summariseCohortAttrition(cdm$vaccine_camp_fin)
 results[["attrition_a"]] <- summariseCohortAttrition(cdm$all_denom)
 
